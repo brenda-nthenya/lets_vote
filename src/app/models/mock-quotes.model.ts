9baa32c72dcab8ecb6ef25682758148294f0b89f
Brenda <bee@bee.com>
2022-04-12 16:30:28 +0300

    Sample of quotes to work with

---
+++ src/app/models/mock-quotes.model.ts
@@ -0,0 +1,12 @@
+import { Quote } from './quotes.model';
+
+export const  QUOTES: Quote [] =[
+  {description:'Life is good',author:'John'},
+  {description:'Life is life',author:'Jay'},
+  {description:'Life is flow',author:'Jace'},
+  {description:'Flow is good',author:'James'},
+  {description:'Flow is life',author:'Jonah'},
+  {description:'Life is life',author:'Amy'},
+  {description:'Flow is flow',author:'Cal'}
+  
+];
